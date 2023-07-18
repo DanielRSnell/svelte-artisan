43aba21e9997b490b6d63e3d55676945641a710f
gitprompting <daniel@domartisan.com>
2023-07-18 15:56:49 -0400

    fixes

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,13 +11,12 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false,
-			force: false
-		}),
+            // See below for an explanation of these options
+            routes: {
+                include: ['/*'],
+                exclude: ['<all>']
+            }
+        }),
 		prerender: {
 			concurrency: 5, // The number of pages that can be prerendered simultaneously.
 			crawl: true, // Whether SvelteKit should find pages to prerender by following links from entries.
