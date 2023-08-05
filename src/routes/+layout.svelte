8526984a0e9f3f00a7508f436207c437781be72c
Daniel Snell <danielsnell@Daniels-MacBook-Pro.local>
2023-08-05 09:31:44 -0700

    updated build command

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,13 @@
 <script lang='ts'>
 	import { browser } from '$app/environment';
 	import '../app.css';
-	import '../bricks.css';
-	import '../automatic.css';
-	import '../frames.css';
 </script>
 
 
 	<svelte:head>
 {#if browser}
 	<script src="https://unpkg.com/hyperscript.org@0.9.11"></script>
-		<script src="https://unpkg.com/htmx.org@1.9.4"></script>
+	<script src="https://unpkg.com/htmx.org@1.9.4"></script>
 {/if}
 	</svelte:head>
 
--- src/routes/[...slug]/+page.server.ts
+++ src/routes/[...slug]/+page.server.ts
@@ -3,7 +3,7 @@ import * as cheerio from 'cheerio';
 
 export async function load({ params }) {
 	const config = {
-		project: 'https://automaticcss.com/', // Fixed typo
+		project: 'https://dev.domartisan.com/', // Fixed typo
 		absolute: false
 	};
 
