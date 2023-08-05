6954732fcc587fe5f9ccc30807e36b747554ee84
Daniel Snell <danielsnell@Daniels-MacBook-Pro.local>
2023-08-04 21:55:43 -0700

    partial build status: endpoint change

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,14 @@
 <script lang='ts'>
-import { browser } from '$app/environment';
-import '../app.css';
-
-	// // The ordering of these imports is critical to your app working properly
-	// import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
-	// // If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
-	// import '@skeletonlabs/skeleton/styles/skeleton.css';
-	// Most of your app wide CSS should be put in this file
-
-	
+	import { browser } from '$app/environment';
+	import '../app.css';
 </script>
+
+
+	<svelte:head>
+{#if browser}
+	<script src="https://unpkg.com/hyperscript.org@0.9.11"></script>
+		<script src="https://unpkg.com/htmx.org@1.9.4"></script>
+{/if}
+	</svelte:head>
 
 <slot />
